b0288d347131eae0a78f7d4882c1270e1454d038
Sandy Gomez <sgomez1029@gmail.com>
2024-04-30 14:23:53 -0700

    Multiple Views in Storyboards

---
+++ MultipleViewsStoryboards/ThirdViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ThirdViewController.swift
+//  MultipleViewsStoryboards
+//
+//  Created by Sandra Gomez on 4/29/24.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    @IBOutlet var myTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- MultipleViewsStoryboards/ViewController.swift
+++ MultipleViewsStoryboards/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
